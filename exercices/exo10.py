e5996ac0a992ff22df44f465af380fa3f7bf3c5d
Julie Sauvage <julie.sauvage@live.fr>
2025-03-13 14:36:52 +0100

    Update exo10.py

--- exercices/exo10.py
+++ exercices/exo10.py
@@ -1,7 +1,3 @@
-# saisie = int(input("saisir un chiffre entre 1 et 3 : "))
-# while (not saisie == 1 or not saisie == 2 or not saisie == 3) :
-#     print("saisie",saisie)
-#     saisie = int(input("saisir un chiffre entre 1 et 3 : "))
 while True: 
     nbr = int(input("saisir un chiffre entre 1 et 3 : "))
     if 1 <= nbr <= 3:
