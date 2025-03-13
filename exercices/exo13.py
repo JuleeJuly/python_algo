4885dfb7c805907275ba0048ca80a801f3f27b06
Julie Sauvage <julie.sauvage@live.fr>
2025-03-13 14:37:26 +0100

    Update exo13.py

--- exercices/exo13.py
+++ exercices/exo13.py
@@ -1,10 +1,3 @@
-# tableau = []
-# for x in range(6):
-#     nbr = int(input("saisir un chiffre : "))
-#     tableau.append(nbr)
-# tableau.sort()
-# print(tableau)
-
 le_plus_grand = 0
 for x in range(6):
     nbr = int(input("saisir un chiffre : "))
